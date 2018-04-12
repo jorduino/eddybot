7b26beff78a7c2b82e61d4e8ac1a6993ebc69c39
Jordan Habash <Jordanhabash@gmail.com>
2018-04-12 10:36:24 -0500

    Moved everything I could into constants.js and reformatted constants.js

--- bot.js
+++ bot.js
@@ -30,10 +30,10 @@ constants.bot.on("message", async message => {
     recentMessage = message;
 
     //ignores messages that the bot sends and that are DM'ed
-    if (message.author.bot || message.channel.type === "dm") return;
+    if (message.author.bot || message.channel.type === "dm") return
 
     //console logs what the message was and who said it on which channel
-    console.log(constants.HR + "|" + message.author.username + ' said :\n|\n| "' + message.content + '"\n|\n| on ' + message.channel.name + constants.HR);
+    constants.printMsg(message);
 
     /*
 		seperates the message into more useable parts
@@ -46,69 +46,37 @@ constants.bot.on("message", async message => {
     let command = messageArray[0];
     let args = messageArray.slice(1);
 
-    //initializes variable that will later hold all 
-    //trigger words that have been said in the message 
-    //recieved (selected from variable slutWords)
-    let words = "";
-
-    //used for comma placement
-    let first = true;
-
-    if (command == 'eddybot') { //"eddybot" commands:
-        if (args.length == 1 && args[0] == "help") { //"eddybot help":
-            message.channel.send('I am eddybot. Current commands:\n' + constants.commands());
-        } else if (args.length > 1 && args[0] == "help") { //"eddybot help ...":
-            let output = "";
-            for (let i = 1; i < args.length; i++) {
-                output += args[i] + " ";
-            }
-            message.channel.send("Nothing to be done for `" + output + "`");
-        }
-
-        if (args.length == 2 && args[0] == "messages") { //"eddybot messages":
-            msgLeft = parseInt(args[1]);
-        }
-        if (message.content.toUpperCase().includes(" OR ")) { //"eddybot ... or ...":
-            message.channel.send(message.content.split(" ")[(Math.random() >= .5 ? message.content.toUpperCase().split(" ").indexOf("OR") - 1 : message.content.toUpperCase().split(" ").indexOf("OR") + 1)]);
-        }
-    }
-
-    for (word of constants.slutWords) {
-        if (message.content.toUpperCase().includes(word.toUpperCase())) {
-            if (first) {
-                first = false;
-                words += word;
+    if (command.toUpperCase() == 'EDDYBOT') { //"eddybot" commands:
+        if (args.length > 0) {
+            if (args[0].toUpperCase() == "HELP") {
+                if (args.length == 1) { //"eddybot help":
+                    message.channel.send('I am eddybot. Current commands:\n' + constants.commands());
+                } else { //"eddybot help ...":
+                    let output = "";
+                    for (let i = 1; i < args.length; i++) {
+                        output += args[i] + " ";
+                    } //end for
+                    message.channel.send("Nothing to be done for `" + output + "`");
+                } //end if
+            } else if (args.length == 2 && args[0].toUpperCase() == "MESSAGES") { //"eddybot messages":
+                msgLeft = parseInt(args[1]);
+            } else if (message.content.toUpperCase().includes(" OR ")) { //"eddybot ... or ...":
+                message.channel.send(message.content.split(" ")[(Math.random() >= .5 ? message.content.toUpperCase().split(" ").indexOf("OR") - 1 : message.content.toUpperCase().split(" ").indexOf("OR") + 1)]);
             } else {
-                words += ", and " + word;
+                message.channel.send("unknown command");
             }
-        }
-    }
-    if (words != "") {
-        message.channel.send(`I'm such a slut for ${words}`);
-    }
-
-    for (array of constants.searchWords) {
-        for (word of array[0]) {
-            if (message.content.toUpperCase().includes(word)) {
-                message.channel.send(array[1]);
-                return;
-            }
-
+        } else {
+            message.channel.send("too few arguments");
         }
     }
 
+    constants.findWords(message);
 
-
-    if (command.toUpperCase() == "RIP" && args > 0) {
-        message.channel.send("Here lies " + args[0] + "\nThey will be missed\n" + ((args.length == 3) ? ("Born:" + args[1] + "\nDied:" + args[2]) : ("Born:420\nDied:6969")));
+    if (command.toUpperCase() == "RIP" && args.length > 0) {
+        constants.rip(message, args)
     }
 
-    if (msgLeft <= 0) {
-        msgLeft = parseInt(constants.random(1, 500));
-        message.channel.send("I eat ass");
-    } else {
-        msgLeft--;
-    }
+    constants.checkMessagesLeft(message);
 
 });
 
@@ -121,4 +89,5 @@ stdin.addListener("data", function (d) {
         console.log("No message selected...")
     }
 });
+
 constants.bot.login(constants.botsettings.token);
--- constants.js
+++ constants.js
@@ -8,74 +8,142 @@ let strCommands = [
     "a few secret commands (i.e. your mom gay, salad, etc)",
     "rip <person> <born> <died> (default: born=420, died=6969)"
 ]
-module.exports = {
-    botsettings: require("../../GitHub private/botsettings.json"),
-    figlet: require("figlet"),
-    HR: "\n-----------------------------------------------\n",
-    slutWords: ["salad", "visual studio", "sluts", "visual studio code", "python", "javascript"],
-    searchWords: [
-        [
-            ["@EVERYONE"],
-            [everyoneLink]
-        ],
-        [
-            ["NO U ^∞"],
-            [":b:oof"]
-        ],
-        [
-            ["NO U ^", "NO U^"],
-            ["No u ^∞"]
-        ],
-
-        [
-            ["NO U"],
-            ["No u ^2"]
-        ],
-        [
-            ["YOUR MOM GAY", "UR MOM GAY"],
-            ["ur dad lesbian"]
-        ],
-        [
-            ["YOUR DAD LESBIAN", "UR DAD LESBIAN"],
-            ["ur sister a mister"]
-        ],
-        [
-            ["YOUR SISTER A MISTER", "UR SISTER A MISTER"],
-            ["ur brother a mother"]
-        ],
-        [
-            ["YOUR BROTHER A MOTHER", "UR BROTHER A MOTHER"],
-            ["ur grandpap a trap"]
-        ],
-        [
-            ["YOUR GRANDPAP A TRAP", "UR GRANPAP A TRAP"],
-            ["ur granny a tranny"]
-        ],
-        [
-            ["YOUR GRANNY A TRANNY", "UR GRANNY A TRANNY"],
-            ["ur ancestors incestors"]
-        ],
-        [
-            ["YOUR ANCESTORS INCESTORS", "UR ANCESTORS INCESTORS"],
-            ["ur family tree LGBT"]
-        ],
-        [
-            ["YOUR FAMILY TREE LGBT", "UR FAMILY TREE LGBT"],
-            ["*dies*"]
-        ]
+let slutWords = ["salad", "visual studio", "sluts", "visual studio code", "python", "javascript"];
+let searchWords = [
+    [
+        ["@EVERYONE"],
+        [everyoneLink]
     ],
-    bot: new Discord.Client({
-        disableEveryone: true
-    }),
-    random: function (min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    },
+    [
+        ["NO U ^∞"],
+        [":b:oof"]
+    ],
+    [
+        ["NO U ^", "NO U^"],
+        ["No u ^∞"]
+    ],
 
-    commands: function () {
-        let output = "";
-        for (command of strCommands) {
-            output += "`" + command + "`" + "\n";
-        }
-        return output;
+    [
+        ["NO U"],
+        ["No u ^2"]
+    ],
+    [
+        ["YOUR MOM GAY", "UR MOM GAY"],
+        ["ur dad lesbian"]
+    ],
+    [
+        ["YOUR DAD LESBIAN", "UR DAD LESBIAN"],
+        ["ur sister a mister"]
+    ],
+    [
+        ["YOUR SISTER A MISTER", "UR SISTER A MISTER"],
+        ["ur brother a mother"]
+    ],
+    [
+        ["YOUR BROTHER A MOTHER", "UR BROTHER A MOTHER"],
+        ["ur grandpap a trap"]
+    ],
+    [
+        ["YOUR GRANDPAP A TRAP", "UR GRANPAP A TRAP"],
+        ["ur granny a tranny"]
+    ],
+    [
+        ["YOUR GRANNY A TRANNY", "UR GRANNY A TRANNY"],
+        ["ur ancestors incestors"]
+    ],
+    [
+        ["YOUR ANCESTORS INCESTORS", "UR ANCESTORS INCESTORS"],
+        ["ur family tree LGBT"]
+    ],
+    [
+        ["YOUR FAMILY TREE LGBT", "UR FAMILY TREE LGBT"],
+        ["*dies*"]
+    ]
+];
+let botsettings = require("../../GitHub private/botsettings.json");
+let figlet = require("figlet");
+let HR = "\n-----------------------------------------------\n";
+let bot = new Discord.Client({
+    disableEveryone: true
+});
+let random = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let commands = function () {
+    let output = "";
+    for (command of strCommands) {
+        output += "`" + command + "`" + "\n";
     }
+    return output;
+};
+let findWords = function (message) {
+    //initializes variable to hold trigger
+    //words found in the message recieved
+    let words = "";
+
+    //used for comma placement
+    let first = true;
+
+    for (word of slutWords) {
+        if (message.content.toUpperCase().includes(word.toUpperCase())) {
+            if (first) {
+                first = false;
+                words += word;
+            } else {
+                words += ", and " + word;
+            }
+        }
+    }
+    if (words != "") {
+        message.channel.send(`I'm such a slut for ${words}`);
+    }
+
+    for (array of searchWords) {
+        for (word of array[0]) {
+            if (message.content.toUpperCase().includes(word)) {
+                message.channel.send(array[1]);
+                return;
+            }
+
+        }
+    }
+};
+let printMsg = function (message) {
+    console.log(
+        HR +
+        "|" + message.author.username + ' said :' +
+        '\n|' +
+        '\n| "' + message.content + '"' +
+        '\n|' +
+        '\n| on ' + message.channel.name +
+        HR
+    );
+};
+let rip = function (message, args) {
+    message.channel.send("Here lies " + args[0] + "\nThey will be missed\n" + ((args.length == 3) ? ("Born:" + args[1] + "\nDied:" + args[2]) : ("Born:420\nDied:6969")));
+};
+let checkMessagesLeft = function (message) {
+    if (msgLeft <= 0) {
+        msgLeft = parseInt(random(1, 500));
+        message.channel.send("I eat ass");
+    } else {
+        msgLeft--;
+    }
+}
+module.exports = {
+    everyoneLink,
+    Discord,
+    strCommands,
+    slutWords,
+    searchWords,
+    botsettings,
+    figlet,
+    HR,
+    bot,
+    random,
+    commands,
+    findWords,
+    printMsg,
+    rip
 }
