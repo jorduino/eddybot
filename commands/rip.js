c816d53fdf5625a5a73f747cc129eaa1a06a4e7a
Jordan Habash <jordanhabash@gmail.com>
2022-10-30 00:21:32 -0500

    updated "rip" command to accept strings

--- commands/rip.js
+++ commands/rip.js
@@ -4,20 +4,20 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('rip')
         .setDescription('custom tombstone')
-        .addUserOption(option =>
+        .addStringOption(option =>
             option.setName('user')
                 .setDescription('User on the tombstone')
                 .setRequired(true))
-        .addNumberOption(option =>
+        .addStringOption(option =>
             option.setName('born')
                 .setDescription('Year they were born'))
-        .addNumberOption(option =>
+        .addStringOption(option =>
             option.setName('died')
                 .setDescription('Year they died')),
     async execute(interaction) {
-        const user = interaction.options.getUser('user').username;
-        const born = interaction.options.getNumber('born') || 420;
-        const died = interaction.options.getNumber('died') || 6969;
+        const user = interaction.options.getString('user');
+        const born = interaction.options.getString('born') || 420;
+        const died = interaction.options.getString('died') || 6969;
 
         interaction.reply("Here lies " + user + "\nThey will be missed\n" + "Born:" + born + "\nDied:" + died);
 
