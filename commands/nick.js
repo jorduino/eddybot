e871a2371eaf0403b422c6493322f5f4b3931f51
jorduino <jordanhabash@gmail.com>
2026-01-10 14:05:28 -0500

    (feat): added nickname command

---
+++ commands/nick.js
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder, MessageFlags } from 'discord.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('nick')
+		.setDescription('Set the nickname of the tagged user(s).')
+		.addUserOption(option => option.setName('user')
+			.setDescription('User to set nickname')
+			.setRequired(true)
+		).addStringOption(option =>
+			option.setName('nickname')
+				.setDescription("New nickname")
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		const targetUser = interaction.options.getUser('user');
+		const newNickname = interaction.options.getString('nickname');
+		const guildMember = await interaction.guild.members.fetch(targetUser.id); // Get the GuildMember object
+
+		try {
+			await guildMember.edit({ nick: newNickname }, 'Bot changed the nickname');
+			await interaction.reply({ content: `Successfully changed ${targetUser}'s nickname to ${newNickname}` });
+		} catch (error) {
+			console.error("Couldn't change nickname\n" + error);
+			await interaction.reply({ content: 'I do not have permission to change that nickname.', flags: MessageFlags.Ephemeral });
+		}
+	},
+};
