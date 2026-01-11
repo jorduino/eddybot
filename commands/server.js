9cf3057bb793d41808095dc066da622e840c065e
jorduino <jordanhabash@gmail.com>
2026-01-11 12:21:13 -0500

    chore: enforced guild-only commands, updated formatting

--- commands/server.js
+++ commands/server.js
@@ -1,11 +1,13 @@
-import { SlashCommandBuilder } from 'discord.js'
-
+import { InteractionContextType, SlashCommandBuilder } from "discord.js";
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Display info about this server.'),
+		.setName("server")
+		.setDescription("Display info about this server.")
+		.setContexts([InteractionContextType.Guild]),
 	async execute(interaction) {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	}
+		await interaction.reply(
+			`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`,
+		);
+	},
 };
