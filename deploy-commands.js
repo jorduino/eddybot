244e4d1972cb9569cd576923bbc126918319e5ed
jorduino <jordanhabash@gmail.com>
2026-01-11 12:08:36 -0500

    chore: removed guildId requirement from deploy-commands.js, updated formatting

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const { clientId, guildId, token } = config;
+const { clientId, token } = config;
 export default async () => {
 	const commands = [];
 	const commandsPath = path.join(__dirname, 'commands');
