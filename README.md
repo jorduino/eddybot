cd0273f6a1cb621d06f411f0b5c03f4d13545a3f
jorduino <jordanhabash@gmail.com>
2026-01-11 12:47:45 -0500

    chore: updated formatting

--- README.md
+++ README.md
@@ -6,12 +6,14 @@ This is a discord bot originally based off of my friend Eddy.
 
 ## Usage
 
-Go to [https://discord.com/developers/applications](https://discord.com/developers/applications) and create an application, then copy the token and client id and put it into a config.json file like the following:
+Go to [https://discord.com/developers/applications](https://discord.com/developers/applications) and
+create an application, then copy the token and client id and put it into a config.json file like the
+following:
 
 ```json
 {
- "token":"token-from-discord-developers-page",
- "clientId":"application-id-from-discord-developers-page"
+	"token": "token-from-discord-developers-page",
+	"clientId": "application-id-from-discord-developers-page"
 }
 ```
 
@@ -19,4 +21,5 @@ Then `bun install` and `bun start`
 
 ## Commands
 
-All commands have been updated to the new slash commands that discord is now using with autocomplete. They can also be found in the `/commands` folder
+All commands have been updated to the new slash commands that discord is now using with
+autocomplete. They can also be found in the `/commands` folder
--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 // TODO: add unit tests
 // TODO: server vs dm commands
 
-import fs from 'node:fs'
-import path from 'node:path'
-import Figlet from 'figlet'
-import deploy_commands from './deploy-commands.js'
-import { Client, Collection, GatewayIntentBits, MessageFlags } from 'discord.js'
-import config from './config.json' with { type: 'json' };
+import fs from "node:fs";
+import path from "node:path";
+import Figlet from "figlet";
+import deploy_commands from "./deploy-commands.js";
+import { Client, Collection, GatewayIntentBits, MessageFlags } from "discord.js";
+import config from "./config.json" with { type: "json" };
 const token = config.token;
-import { printSentMessage } from './printMsg.js'
-import { fileURLToPath } from 'node:url';
+import { printSentMessage } from "./printMsg.js";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,8 +19,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 await deploy_commands();
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -29,31 +29,36 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-client.once('clientReady', async () => {
-	//prints bot's name in large ascii
-	// console.clear();
-	Figlet(client.user.username, function (err, data) {
-		if (err) {
-			console.log(constants.bot.user.username.toUpperCase());
-			console.dir(err);
-			return;
+client
+	.once("clientReady", async () => {
+		//prints bot's name in large ascii
+		// console.clear();
+		Figlet(client.user.username, function (err, data) {
+			if (err) {
+				console.log(constants.bot.user.username.toUpperCase());
+				console.dir(err);
+				return;
+			}
+			console.log(data);
+			console.log("Bot is ready");
+		});
+	})
+	.on("interactionCreate", async interaction => {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) return;
+
+		try {
+			// printSentMessage(interaction)
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({
+				content: "There was an error while executing this command!",
+				flags: MessageFlags.Ephemeral,
+			});
 		}
-		console.log(data);
-		console.log("Bot is ready");
-	});
-}).on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		// printSentMessage(interaction)
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-	}
-})
+	})
 	.login(token);
