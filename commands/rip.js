b8ce123f05bef891e105922d78acc35cb9854488
jorduino <jordanhabash@gmail.com>
2026-01-11 12:20:46 -0500

    chore: updated formatting

--- commands/rip.js
+++ commands/rip.js
@@ -1,25 +1,21 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { SlashCommandBuilder } from "discord.js";
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('rip')
-		.setDescription('custom tombstone')
+		.setName("rip")
+		.setDescription("custom tombstone")
 		.addStringOption(option =>
-			option.setName('user')
-				.setDescription('User on the tombstone')
-				.setRequired(true))
-		.addStringOption(option =>
-			option.setName('born')
-				.setDescription('Year they were born'))
-		.addStringOption(option =>
-			option.setName('died')
-				.setDescription('Year they died')),
+			option.setName("user").setDescription("User on the tombstone").setRequired(true),
+		)
+		.addStringOption(option => option.setName("born").setDescription("Year they were born"))
+		.addStringOption(option => option.setName("died").setDescription("Year they died")),
 	async execute(interaction) {
-		const user = interaction.options.getString('user');
-		const born = interaction.options.getString('born') || 420;
-		const died = interaction.options.getString('died') || 6969;
+		const user = interaction.options.getString("user");
+		const born = interaction.options.getString("born") || 420;
+		const died = interaction.options.getString("died") || 6969;
 
-		interaction.reply("Here lies " + user + "\nThey will be missed\n" + "Born:" + born + "\nDied:" + died);
-
-	}
-}
+		interaction.reply(
+			"Here lies " + user + "\nThey will be missed\n" + "Born:" + born + "\nDied:" + died,
+		);
+	},
+};
--- commands/source.js
+++ commands/source.js
@@ -1,10 +1,10 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { SlashCommandBuilder } from "discord.js";
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('source')
-		.setDescription('Gives source code for eddybot'),
+		.setName("source")
+		.setDescription("Gives source code for eddybot"),
 	execute(interaction) {
-		interaction.reply('Heres my source code!\nhttps://github.com/jorduino/eddybot.git')
-	}
-}
+		interaction.reply("Heres my source code!\nhttps://github.com/jorduino/eddybot.git");
+	},
+};
