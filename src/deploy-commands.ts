785f789217e387d76fa92899963cf3e38c2189bf
jorduino <jordanhabash@gmail.com>
2026-01-23 01:59:47 -0500

    chore: bundled binary (#38)
    * chore: create static commands index
    
    * chore: fix issue with figlet
    
    * chore: create scripts
    
    * chore: added release workflow for github
    
    * chore: bump version number
    
    * chore: swapped to environment variables and updated readme
    
    * chore: added deployment script in release action
    
    * chore: bump version number

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,27 +1,16 @@
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { REST } from "@discordjs/rest";
 import type { RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord.js";
 import { Client, Collection, Routes } from "discord.js";
-import type { Command, Config } from "./types/commands"; // wherever you put them
+import allCommands from "./commands";
+import type { Command, Config } from "./types/commands";
 import printCommands from "./util/printCommands";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default async (client: Client, config: Config) => {
 	const { clientId, token } = config;
 	const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
 	client.commands = new Collection<string, Command>();
-	const commandsPath = path.join(__dirname, "commands");
-	const commandFiles = fs
-		.readdirSync(commandsPath)
-		.filter(file => file.endsWith(".js") || file.endsWith(".ts"));
 
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const { default: command } = await import(filePath);
+	for (const command of allCommands) {
 		commands.push(command.data.toJSON());
 		client.commands.set(command.data.name, command);
 	}
